2c3617868553671fa59c5e9314216a10788e3f15
Harman <harmandeepsingh7@student.mitt.ca>
2026-06-07 03:45:30 -0500

    addedd useState and click functionality to starrating

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import Star from "./Star";
 
 function StarRating() {
+  const [rating, setRating] = useState(0);
+
+  function handleClick(index) {
+    setRating(index);
+  }
+
   return (
     <div className="star-container">
       {[1, 2, 3, 4, 5].map((item, index) => (
-        <Star key={index} />
+        <Star
+          key={index}
+          selected={index < rating}
+          onClick={() => handleClick(index + 1)}
+        />
       ))}
     </div>
   );
