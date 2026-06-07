6359d022a4522143e42edcc0be7796d737117ca5
Harman <harmandeepsingh7@student.mitt.ca>
2026-06-07 04:07:59 -0500

    added rating text based on selected stars and adjusted their position

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -8,17 +8,30 @@ function StarRating() {
     setRating(index);
   }
 
+function getText(rating) {
+    if (rating === 1) return "Poor";
+    if (rating === 2) return "Fair";
+    if (rating === 3) return "Good";
+    if (rating === 4) return "Very Good";
+    if (rating === 5) return "Excellent";
+    return "";
+    }
+
   return (
     <div className="star-container">
-      {[1, 2, 3, 4, 5].map((item, index) => (
-        <Star
-          key={index}
-          selected={index < rating}
-          onClick={() => handleClick(index + 1)}
-        />
-      ))}
+        <h2 className="rating-text">{getText(rating)}</h2>
+
+        <div className="stars">
+        {[1, 2, 3, 4, 5].map((item, index) => (
+            <Star
+            key={index}
+            selected={index < rating}
+            onClick={() => handleClick(index + 1)}
+            />
+        ))}
+        </div>
     </div>
-  );
+);
 }
 
 export default StarRating;
